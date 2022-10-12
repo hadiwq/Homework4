432e25ad8af56be0e00ea48e01c2d675f6281fd6
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-12 13:59:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,3 +11,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+$servername = "165.227.18.177";
+$username = "alqadiou";
+$password = "1[L0B2U1kkqXh*";
+$dbname = "alqadiou_homework4";
+?>
