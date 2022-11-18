b6e0128d0612c10643bc21610e0f788d72c9ada3
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-17 22:26:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
-    
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
       <h1>Instructors</h1>
       <table class="table table-striped">
         <thead>
